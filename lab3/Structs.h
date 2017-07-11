6d6a3db15025d8c6d0973676d232f9e0aa7c35ac
AJIOB <aleksandr9809@gmail.com>
2017-07-11 09:54:09 +0300

    travis lab1 first build

---
+++ lab3/Structs.h
@@ -0,0 +1,28 @@
+#ifndef STRUCTS
+#define STRUCTS
+
+#include <string>
+
+struct Command
+{
+	bool isAdd;	//true = add;  fasle == delete
+	std::string name;
+
+	Command()
+	{
+		isAdd = false;
+	}
+
+	Command(bool isAdd, std::string name)
+	{
+		this->isAdd = isAdd;
+		this->name = name;
+	}
+
+	Command(const Command& c)
+	{
+		isAdd = c.isAdd;
+		name = c.name;
+	}
+};
+#endif
---
+++ lab4/code/ThreadManager.h
@@ -0,0 +1,97 @@
+#ifndef THREAD_MANAGER
+#define THREAD_MANAGER
+
+#ifdef _WIN32
+
+#include <deque>
+#include "logs.h"
+#include "Sync.h"
+
+
+#elif (defined(__linux__) || defined(__unix__))
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <pthread.h>
+#include <list>
+#include "Thread.h"
+
+#else
+#error Bad operation system. Please, recompile me to Linux, Unix or Windows
+#endif
+
+#include "ThreadFuncs.h"
+
+class ThreadManager
+{
+#ifdef _WIN32
+
+	std::deque<Sync *> flags;
+	CRITICAL_SECTION workWithFlags;
+
+	friend DWORD WINAPI threadGenerator(LPVOID);
+	friend DWORD WINAPI threadPrinter(LPVOID);
+	friend int calculateCurrentPrintIndex(ThreadManager* manager, int prevIndex);
+
+	HANDLE printerThread;
+	HANDLE generatorThread;
+
+	const double showInterval; //interval between printing names
+	const double createNewThreadInterval; //interval between creating threads
+	bool isStopGeneration;
+	bool isStopPrinting;
+
+public:
+	ThreadManager(const double& showInterval, const double& createNewThreadInterval);
+	~ThreadManager();
+
+	void generateNewThread();
+	bool removeThread(); //kill random thread
+	bool removeThread(int index);
+
+	void runGeneration();
+	void stopGeneration();
+
+	void runPrinting();
+	void stopPrinting();
+
+	void runAll();
+	void stopAll();
+
+	int getNumOfThreads() const;
+
+#elif (defined(__linux__) || defined(__unix__))
+
+    pthread_t printerThread;
+    pthread_t generatorThread;
+
+    pthread_mutex_t lock;
+
+public:
+
+	bool printerAlive;
+	bool generatorAlive;
+
+	double printerInterval;
+	double generatorInterval;
+
+	char threadName;
+	std::list <Thread *> runningThreads;
+
+
+	ThreadManager(const double& printerInterval_, const double& generatorInterval_);
+	~ThreadManager();
+
+	void generateNewThread();
+	void removeThread();			//kills first thread
+
+	void runAll();
+	void stopAll();
+
+	int getNumOfThreads();
+#endif
+};
+
+#endif
